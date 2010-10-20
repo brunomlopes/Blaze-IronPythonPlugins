6d7617f87cfd2a8991de2151e487c1b04fab8c4c
brunomlopes <brunomlopes@gmail.com>
2010-10-20 09:14:28 +0100

    Small change to build script

--- default.ps1
+++ default.ps1
@@ -115,5 +115,6 @@ Task CopyLocal -depends Build {
     }
     Copy-Item "source\bin\$configuration\*" -destination "D:\documents\My Dropbox\blaze\Plugins" -recurse
     Copy-Item "source\IronPythonPlugins\*" -destination "D:\documents\My Dropbox\blaze\Plugins\IronPythonPlugins"
+    Write-Host -ForegroundColor Cyan "    Starting blaze with new version of plugin" 
     Start-Process "D:\documents\My Dropbox\blaze\Blaze.exe" -WorkingDirectory "D:\documents\My Dropbox\blaze\"
 }
